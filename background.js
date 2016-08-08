900b4ad12f911c5ab22c4272a4a17ce5225c32bb
danshiner <danshiner@gmail.com>
2016-08-08 18:53:01 -0400

    Barebones menubar working: appears, opens prefs window, quits

--- background.js
+++ background.js
@@ -1,47 +1,29 @@
+const menubar = require('menubar')
+const ipcMain = require('electron').ipcMain
+const {app, BrowserWindow} = require('electron') //note I need the app so that I can call app.quit(); --> maybe a cleaner way to do this
 
-const {app, BrowserWindow} = require('electron')
+let options = {
+  index: `file://${__dirname}/views/index.html`,
+};
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+var mb = menubar(options);
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+mb.on('ready', function ready () {
+  console.log('app is ready')
 
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/public/index.html`)
+  // When show-prefs is sent, create a new browser window for preferences.
+  ipcMain.on('show-prefs', function(){
+    let prefsWindow = new BrowserWindow({
+      width: 400,
+      height: 400,
+    });
+    prefsWindow.loadURL(`file://${__dirname}/views/prefs.html`);
+  });
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
-}
+  // When quit is sent, quit the app.
+  ipcMain.on('quit', function(){
+    app.quit();
+  });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
 })
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
