c773eddaff7507b92f072b2807c1359c34f097ba
danshiner <danshiner@gmail.com>
2016-08-10 15:56:15 -0400

    adding message storage and menubar notifications

--- services/messages.js
+++ services/messages.js
@@ -1,9 +1,22 @@
-module.exports = {
-    addMessage : function(message) { messages.message.user = message },
-    removeMessage : function(message) { delete messages.message },
+function addMessage(message){
+  currentMessages[message.user] = message;
+}
+
+function deleteMessage(message){
+  delete currentMessages[message.user] = message;
+}
+
+var currentMessages = {
     'U1Z0MHRGB' :  {"type":"message","channel":"C1YSPCUQK","user":"U1Z0MHRGB","text":"#now","ts":"1470790572.000044","team":"T1YU0S4TE"},
     'U1Z0MHRGB' :  {"type":"message","channel":"C1YSPCUQK","user":"U1Z0MHRGB","text":"hello #now","ts":"1470790519.000043","team":"T1YU0S4TE"}
 }
+
+module.exports = {
+  addMessage : addMessage,
+  deleteMessage : deleteMessage,
+  currentMessages : currentMessages
+};
+
 
 // VS EXPORT?
 // sample message for reference {"type":"message","channel":"C1YSPCUQK","user":"U1Z0MHRGB","text":"#now","ts":"1470790572.000044","team":"T1YU0S4TE"}
--- services/websockets.js
+++ services/websockets.js
@@ -18,9 +18,14 @@ function launchRtmClient(){
   });
 
   // Create events listener, including notifications
-  rtm.on(RTM_EVENTS.MESSAGE, notifications.notifyMain);
+
+  rtm.on(RTM_EVENTS.MESSAGE, currentMessages.addMessage);
+  // setTimeout(test, 6000);
+  // rtm.on(RTM_EVENTS.MESSAGE, notifications.notifyMain);
 
 }
+// function test(){console.log('here is the message:'+JSON.stringify(messages.currentMessages))}
+
 
 // Come back and add multi-OS support when you have time https://github.com/mikaelbr/node-notifier#use-inside-tmux-session
 
